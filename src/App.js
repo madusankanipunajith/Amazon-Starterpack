aaf8537f97016e13b418ea6ade586610925c92c2
Madusanka Nipunajith <56679833+madusankanipunajith@users.noreply.github.com>
2022-11-18 13:07:23 +0530

    Add Virtual section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 import Header from './components/header/Header';
 import Hero from "./components/hero/Hero";
+import Products from "./components/products/Products";
 import Slider from "./components/slider/Slider";
+import Virtual from "./components/virtual/Virtual";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Header/>
       <Hero/>
       <Slider/>
+      <Virtual/>
+      <Products/>
     </div>
   );
 }
